ebf8858177ff603670b0e8370e5afa49ede1248a
kanika <kanika@example.com>
2021-11-20 00:40:58 +0530

    Code merged

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,11 @@ function Header() {
     return (
         <div className="header">
             <div className="logo">CARE</div>
-            <div  classNAme="logo_image" img src=""></div>
-            <div className="logo_image">
-                <img src=""></img>
-                <div className="nav_elements">
+            <div className="nav_elements">
                     <div className="about">About Us</div>
                     <div className="help">Help</div>
                 </div>
             </div>
-            
-        </div>
     )
 }
 
