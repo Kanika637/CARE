617ac12ccb478ff51d6b1fbc11d5e63aef27cfcd
kanika <kanika@example.com>
2021-11-21 22:20:08 +0530

    image updating

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
-||||||| 8187b40
 import './App.css';
-import "./header"
-=======
-
->>>>>>> upstream/main
+import "./header.css"
 import Header from "./Header"
-// import Image from "./Image"
+import Image from "./Image"
 import Login from "./Login"
-
-
-  return (
-<<<<<<< HEAD
+function App(){
+return (
     <Router>
       <div className="app">
         <Routes>
           <Route exact path="/" element={<Header/>}></Route>
           <Route exact path="/login" element={<Login/>}></Route>
-    {/* <Image /> */}
-    </Routes>
-||||||| 8187b40
-    <div>
-     <Header/>
-    <Image />
-=======
-    <div>
     
-     <Header/>,
+    </Routes>
+    
     <Image />,
     
->>>>>>> upstream/main
     </div>
     </Router>
     
+   
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./header.css"
+
 function Header() {
     return (
         <div className="header">
             <div className="logo">CARE</div>
             <div className="nav_elements">
+            <div className="sign">Sign In</div>
                     <div className="about">About Us</div>
                     <div className="help">Help</div>
                 </div>
